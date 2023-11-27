74d524ce28657579d89a0b62bd390506cf0a7b89
Emir Tunahan Alim <emrtnhalim@gmail.com>
2023-11-28 01:36:36 +0300

    Update layout naming and structure in `test.ui` and `test.py`
    Changed the name of `horizontalLayout` to `upperLayout` in `test.ui` and `test.py` to reflect its actual position in the UI. In the same file, added `contentlayout` and `playerlayout` to `verticalLayout_2` which will be used for future UI enhancements. These changes will improve the clarity of layout positioning and make UI’s future modifications easier.

--- test.py
+++ test.py
@@ -9,13 +9,15 @@ app = QApplication([])
 
 window = uic.loadUi("test.ui")  # Load the PyQt UI file
 
-# Assuming horizontalLayout is a layout in window, retrieve it like this:
-horizontalLayout = window.horizontalLayout
+# Get the upperlayout from the PyQt UI file
+upperLayout = window.upperLayout
+
 # set maximum height for each widget in the layout
-for i in range(horizontalLayout.count()):
-    widget = horizontalLayout.itemAt(i).widget()
+for i in range(upperLayout.count()):
+    widget = upperLayout.itemAt(i).widget()
     if widget is not None:
         widget.setMaximumHeight(40)
+
 
 window.show()
 app.exec_()
