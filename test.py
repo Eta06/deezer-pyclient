1a3702019251547ab17b0262ce3edc519484aab1
Emir Tunahan Alim <emrtnhalim@gmail.com>
2023-11-27 23:27:37 +0300

    In development client

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import QApplication
 
 app = QApplication([])
-window = uic.loadUi("test.ui")  # PyQt Designer ile oluturulan .ui dosyanızın adını yazınız.
-
+window = uic.loadUi("test.ui") # Load the PyQt UI file
 
 
 window.show()
