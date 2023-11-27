f7f1247dc4c9d41042089c19ee613abb23ce718e
Emir Tunahan Alim <emrtnhalim@gmail.com>
2023-11-28 00:02:08 +0300

    Add user flow and initialization in deezertools
    Added a function 'get_user_flow' to fetch the user's music recommendations based on listening history, favorited tracks, and playlists. This function should improve user experience by providing personalized content. An 'init' function which encapsulates user authentication, playlist retrieval, user data fetching within a single function is added too. It provides cleaner and more manageable code, which helps in readability and maintenance. Their implementation was performed on test.py file, reducing the code for easier understanding.

--- deezertools.py
+++ deezertools.py
@@ -57,6 +57,21 @@ def get_user_info(session_token, user_id):
         return None
 
 
+def get_user_flow(session_token, user_id):
+    api_url = f"https://api.deezer.com/user/{user_id}/flow"
+    headers = {"Authorization": f"Bearer {session_token}"}
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        user_flow = response.json()
+
+        return user_flow
+    else:
+        print(f"Hata Kodu: {response.status_code}, Hata Mesajı: {response.text}")
+        return None
+
+
 def read_arl_token():
     """
     Reads arl token from app_config.json file
@@ -65,3 +80,20 @@ def read_arl_token():
     with open("app_config.json", "r", encoding="utf-8") as f:
         arl_token = json.load(f)["arl_token"]
     return arl_token
+
+
+def init():
+
+    arl_token = read_arl_token()
+    session_token, user_id = authenticate_user(arl_token)
+    playlists = get_user_playlists(session_token, user_id)
+    username, user_picture, country = get_user_info(session_token, user_id)
+
+    return {
+        "session_token": session_token,
+        "user_id": user_id,
+        "playlists": playlists,
+        "username": username,
+        "user_picture": user_picture,
+        "country": country
+    }
--- test.py
+++ test.py
@@ -3,16 +3,12 @@ from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import QApplication
 import deezertools as deezer
 
-# Deezer API
-arl_token = deezer.read_arl_token()
-session_token, user_id = deezer.authenticate_user(arl_token)
-playlists = deezer.get_user_playlists(session_token, user_id)
-user_info = deezer.get_user_info(session_token, user_id)
-print(user_info)
+
+user = deezer.init()
 
 app = QApplication([])
 
-window = uic.loadUi("test.ui")  # Load the PyQt UI file
+window = uic.loadUi(f"test.ui".format())  # Load the PyQt UI file
 
 window.show()
 app.exec_()
