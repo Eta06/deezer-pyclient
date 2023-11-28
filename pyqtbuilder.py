38f2467016ce092dfb16d1aadf063c0712a034cd
Emir Tunahan Alim <emrtnhalim@gmail.com>
2023-11-29 00:18:21 +0300

    Updated UI style and implemented hot reloading
    Updated the user interface in test.ui file to improve its appearance by adding new styles and enhanced PyQt builder with threading and time imports.
    
    Removed the 'Build As Python File' button because file change detection in the background was implemented to auto-restart the application when changes are detected in either 'test.py' or 'test.ui'. Thus making the building process automatic and eliminating the need for manual build button.
    
    Also, enhanced functionality by adding hot reloading mechanism using a separate thread. This enables the detection of changes in 'test.ui' and 'test.py' files so that the PyQt application can be restarted automatically when a change is detected.
    
    Updated 'test.py' by adding more functionality to the PyQt application with QPushButtons, which have enriched functionalities with font style and icon amendments.

--- pyqtbuilder.py
+++ pyqtbuilder.py
@@ -1,15 +1,57 @@
 import PySimpleGUI as sg
 import os
+import threading
+import time
 
 sg.theme('DarkAmber')
 
-layout = [[sg.Text("PyQt Builder", size=(27, 2), justification='c', font=15)], [sg.Button('Run', size=(37, 1))],
-          [sg.Button("Build As Python File", size=(37, 1))], [sg.Button("Always On Top", size=(37, 1))],
-          [sg.Button('Exit', size=(37, 1))]]
+layout = [[sg.Text("PyQt Builder", size=(27, 2), justification='c', font=15)], [sg.Button('Run', size=(37, 1))],[sg.Button("Always On Top", size=(37, 1))],
+[sg.Button('Exit', size=(37, 1))]]
 
 window = sg.Window('PyQt Builder', layout)
 
 is_on_top = False
+
+def hot_reload_thread():
+    global last_mtime_test_py, last_mtime_test_ui
+
+    # Initialize last modified times
+    last_mtime_test_py = os.path.getmtime('test.py') if os.path.exists('test.py') else 0
+    last_mtime_test_ui = os.path.getmtime('test.ui') if os.path.exists('test.ui') else 0
+
+    while True:
+        # Check if test.py or test.ui has changed
+        if os.path.exists('test.py') and os.path.getmtime('test.py') > last_mtime_test_py:
+            # Close the current window
+            if event == sg.WIN_CLOSED:
+                hot_reload_thread.exit()
+
+            # Restart the PyQt app
+            os.system('python test.py')
+
+            # Update last modified time
+            last_mtime_test_py = os.path.getmtime('test.py')
+
+        if os.path.exists('test.ui') and os.path.getmtime('test.ui') > last_mtime_test_ui:
+            # Close the current window
+            if event == sg.WIN_CLOSED:
+                hot_reload_thread.exit()
+
+            # Restart the PyQt app
+            os.system('python test.py')
+
+            # Update last modified time
+            last_mtime_test_ui = os.path.getmtime('test.ui')
+
+        # Sleep for a second
+        time.sleep(1)
+
+last_mtime_test_py = 0
+last_mtime_test_ui = 0
+
+# Start hot reload thread
+hot_reload_thread = threading.Thread(target=hot_reload_thread)
+hot_reload_thread.start()
 
 while True:
     event, values = window.read()
@@ -18,11 +60,6 @@ while True:
     if event == 'Run':
         # Run the PyQt app with uic
         os.system('python test.py')
-    if event == 'Build As Python File':
-        # Build the PyQt app with pyuic5 -x test.ui -o test.py
-        os.system('pyuic5 -x test.ui -o test.py')
-        # Run the PyQt app with python test.py
-        os.system('python test.py')
     if event == 'Always On Top':
         if is_on_top:
             window.TKroot.attributes("-topmost", False)
@@ -30,3 +67,6 @@ while True:
         else:
             window.TKroot.attributes("-topmost", True)
             is_on_top = True
+
+# Stop hot reload thread when window is closed
+hot_reload_thread.join()
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from PyQt5 import uic, QtWidgets, QtGui
+from PyQt5 import uic, QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QMovie, QTextOption
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
 import deezertools as deezer
 
 user = deezer.init()
@@ -26,6 +27,43 @@ else:
 
 # <widget class="QListWidget" name="listWidget"> isimli widget'ın içine AnaSayfa, Keşfet, Kütüphane ve Sevilenler butonlarını ekler
 listWidget = window.findChild(QtWidgets.QListWidget, 'listWidget')
+
+button_names = ["AnaSayfa", "Keşfet", "Kütüphane", "Sevilenler"]
+button_icons = ["./assets/play_button_white.png", ":/assets/play_button_white.png", ":/assets/play_button_white.png", ":/assets/play_button_white.png"]
+
+# Check if listWidget exists
+if listWidget is not None:
+    # Create QPushButton elements and add them to the QListWidget
+    for button_name, button_icon in zip(button_names, button_icons):
+        # Create a QListWidgetItem
+        item = QtWidgets.QListWidgetItem()
+
+        # Create a QPushButton
+        button = QtWidgets.QPushButton(button_name)
+
+        # Set the font size
+        font = button.font()
+        font.setPointSize(14)  # 14 is the font size
+        button.setFont(font)
+
+        # Set the icon
+        icon = QtGui.QIcon(button_icon)
+        button.setIcon(icon)
+        # 24,24 is the icon size
+        button.setIconSize(QtCore.QSize(24, 24))
+        button.setStyleSheet("text-align:left;")
+        button.setFlat(True)
+
+        # Set the size of the QListWidgetItem
+        item.setSizeHint(button.sizeHint())
+
+        # Add the QListWidgetItem to the QListWidget
+        listWidget.addItem(item)
+
+        # Set the QPushButton as the widget for the QListWidgetItem
+        listWidget.setItemWidget(item, button)
+else:
+    print("No widget named 'listWidget' found")
 
 
 window.show()
