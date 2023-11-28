3361a0838e29c8a2bf4d28043829dbc20da58b62
Emir Tunahan Alim <emrtnhalim@gmail.com>
2023-11-28 23:51:21 +0300

    "Changed QListView to QListWidget and imported QtGui in test.py"
    Changed the UI class from QListView to QListWidget to better accommodate button lists. This required importing the QtGui module in the test.py file to access the necessary functions and methods.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from PyQt5 import uic, QtWidgets
+from PyQt5 import uic, QtWidgets, QtGui
 from PyQt5.QtGui import QMovie, QTextOption
 from PyQt5.QtWidgets import QApplication
 import deezertools as deezer
@@ -23,6 +23,10 @@ if upperLayout is not None:
             widget.setMaximumHeight(40)
 else:
     print("No layout named 'horizontal_layout1' found")
+
+# <widget class="QListWidget" name="listWidget"> isimli widget'ın içine AnaSayfa, Keşfet, Kütüphane ve Sevilenler butonlarını ekler
+listWidget = window.findChild(QtWidgets.QListWidget, 'listWidget')
+
 
 window.show()
 app.exec_()
